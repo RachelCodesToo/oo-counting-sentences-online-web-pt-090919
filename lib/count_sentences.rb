3c22f701a25e304f32ebd6efe88c8973ebd09167
Anne Fausto <a.r.fausto@gmail.com>
2019-10-06 23:01:25 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,6 +17,9 @@ class String
   def count_sentences
     #self.split(" ").length
     #binding.pry
-    (0..(self.length-1)).inject(1){|m,e| m += self[e].chr == ' ' ? 1 : 0 }
+    #(0..(self.length-1)).inject(1){|m,e| m += self[e].chr == ' ' ? 1 : 0 } *** suggestion from stack overflow but how does it translate?
+    
+    
+    
   end
 end
